59151bafb5904ab0818f74a4c8751171f21fcb95
Ozay BAKIRHAN <ozay.bakirhan@horoz.com.tr>
2022-08-15 12:28:45 +0300

    initial comment

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about page</h1>
+      <p>
+        Laboris exercitation deserunt amet nulla est cillum amet laborum. Non consectetur non mollit est eiusmod non dolor veniam esse. Laboris laborum nisi qui id do minim non. Occaecat culpa mollit est enim aliqua id tempor aliqua sunt in proident mollit in nostrud. Voluptate ad proident anim esse reprehenderit. Dolor consequat nulla id dolor laborum nulla minim dolor ad in enim Lorem dolore in. Non aliquip labore elit incididunt excepteur adipisicing sunt ad cupidatat do occaecat sint.
+      </p>
+    </section>
+  )
+}
+
+export default About
